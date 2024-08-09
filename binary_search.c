8c4ffb0bc015c13469a48159e5489c49e8ffd846
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2024-08-09 06:20:24 +0530

    Add files via upload

---
+++ binary_search.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<ctype.h>
+void main()
+{
+	int n,i,a[10],top,mid,bottom,flag=0,num;
+	printf("Enter the Limit=");
+	scanf("%d",&n);
+	printf("Enter the elements :");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter number to search=");
+	scanf("%d",&num);
+	top=0;
+	bottom=n-1;
+	while(top<=bottom)
+	{
+		mid=(top+bottom)/2;
+	
+		if(a[mid]==num)
+		{
+		flag=1;
+		break;
+		}
+		if(num<a[mid])
+		bottom=mid-1;
+		else
+		top=mid+1;
+		
+	}
+	if(flag==1)
+	printf("Given number is found at=%d ",mid);
+	else
+	printf("oops! number not found...........");
+		
+}
---
+++ each_number_measur_ of.c
@@ -0,0 +1,27 @@
+//occurance of the number
+#include<stdio.h>
+int main()
+{
+	int num,a[10],i,j,n,count=0;
+	printf("enter the limit for the array=");
+	scanf("%d",&n);
+	printf("enter elements in the array=");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);	
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		num=a[i];
+		count=0;
+		for(j=0;j<n;j++)
+			{
+			if(a[j]==num)
+			count++;
+			}
+			printf("\n occurance of the number %d is =%d",num,count);
+	
+	}
+	
+}
