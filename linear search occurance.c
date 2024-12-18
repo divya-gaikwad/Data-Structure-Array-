455f5238b9486569ac5dbbcf085aefb25b743d69
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2024-12-18 09:04:07 +0530

    Update linear search occurance.c

--- linear search occurance.c
+++ linear search occurance.c
@@ -1,15 +1,16 @@
+//search the occurance 
 #include<stdio.h>
 void main()
 {
 	int num,a[10],i,n,count=0;
-	printf("enter the limit for the array=");
+	printf("Enter the limit for the array=");
 	scanf("%d",&n);
-	printf("enter elements in the array=");
+	printf("Enter elements in the array=");
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a[i]);	
 	}
-	printf("enter the number to count occurance=");
+	printf("Enter the number to count occurance=");
 	scanf("%d",&num);
 	
 	for(i=0;i<n;i++)
